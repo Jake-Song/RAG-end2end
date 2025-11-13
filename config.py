997f09c146069051674700883a4c64d1306df56c
Jake Song <mubok0212@gmail.com>
2025-11-14 01:40:14 +0900

    config.py: replace abosulte path to load path dynamically

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 # 설정
-input_file = "data/SPRI_AI_Brief.pdf"  # Replace with a file of your own
+PROJECT_ROOT = Path(__file__).parent.resolve()
+
+input_file = PROJECT_ROOT / "data" / "SPRI_AI_Brief.pdf"  # Replace with a file of your own
 batch_size = 10  # Maximum available value is 100
-image_output_path_prefix = "/images/SPRI_AI_Brief_cropped"
-output_path_prefix = "SPRI_AI_Brief_output"
+image_output_path_prefix = PROJECT_ROOT / "images" / "SPRI_AI_Brief_cropped"
+output_path_prefix = PROJECT_ROOT / "SPRI_AI_Brief_output"
