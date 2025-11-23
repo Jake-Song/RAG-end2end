ac664b4e630a90a7ed93813bc4c6eea99c40876a
Jake Song <mubok0212@gmail.com>
2025-11-23 22:01:43 +0900

    test for fixing retrieval evaluation

---
+++ test/test_conflict_matrix.py
@@ -0,0 +1,65 @@
+def conflict_matrix(retrieved_page_number, reference_page_number):
+    true_positives = 0
+    false_positives = 0
+    false_negatives = 0
+    for i, page in enumerate(retrieved_page_number):
+        if page in reference_page_number:
+            true_positives += 1
+        elif i <= len(reference_page_number)-1 and page not in reference_page_number:
+            false_positives += 1
+            false_negatives += 1
+        elif i > len(reference_page_number)-1 and page not in reference_page_number:
+            false_positives += 1
+            
+    return true_positives, false_positives, false_negatives
+
+# ref page를 전부 찾은 경우
+def test_all_match():
+    retrieved_page_number = [1, 2, 3]
+    reference_page_number = [1, 2, 3]
+
+    true_positives, false_positives, false_negatives = conflict_matrix(retrieved_page_number, reference_page_number)
+
+    assert true_positives == 3
+    assert false_positives == 0
+    assert false_negatives == 0
+
+# ref page를 전부 찾지 못한 경우
+def test_all_no_match():
+    retrieved_page_number = [4, 5, 6]
+    reference_page_number = [1, 2, 3]
+
+    true_positives, false_positives, false_negatives = conflict_matrix(retrieved_page_number, reference_page_number)
+
+    assert true_positives == 0
+    assert false_positives == 3
+    assert false_negatives == 3
+
+# ref page를 전부 찾고 잘 못된 페이지를 찾은 경우
+def test_all_match_but_wrong_page():
+    retrieved_page_number = [1, 2, 3, 4, 6, 7]
+    reference_page_number = [1, 2, 3]
+
+    true_positives, false_positives, false_negatives = conflict_matrix(retrieved_page_number, reference_page_number)
+
+    assert true_positives == 3
+    assert false_positives == 3
+    assert false_negatives == 0
+
+# ref page를 일부 찾고 잘 못된 페이지를 찾은 경우
+def test_partial_match_but_wrong_page():
+    retrieved_page_number = [1, 2, 5, 7]
+    reference_page_number = [1, 2, 3]
+
+    true_positives, false_positives, false_negatives = conflict_matrix(retrieved_page_number, reference_page_number)
+
+    assert true_positives == 2
+    assert false_positives == 2
+    assert false_negatives == 1
+
+
+# 테스트 실행
+test_all_match()
+test_all_no_match()
+test_all_match_but_wrong_page()
+test_partial_match_but_wrong_page()
